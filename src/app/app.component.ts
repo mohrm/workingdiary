38287bf401174799552126421e2bd2cbfb0d19c5
Martin Mohr <martin.mohr@posteo.de>
2025-03-02 14:17:18 +0100

    add ability to go to previous and next day

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,20 @@
-import { Component, input, model, ModelSignal, OnInit, output, OutputEmitterRef } from '@angular/core';
+import { Component, computed, inject, input, model, ModelSignal, OnInit, output, OutputEmitterRef } from '@angular/core';
 import { Section } from './model/Section';
 import { DayPlan } from './feature/day-plan/day-plan';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Params, RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { PersistenceServiceService } from './persistence-service.service';
 
 @Component({
   selector: 'app-root',
-  imports: [DayPlan],
+  imports: [DayPlan, RouterLink],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent implements OnInit {
 
   day = model.required<string>();
+
+  persistence = inject(PersistenceServiceService)
 
   constructor(private route: ActivatedRoute) {}
 
@@ -25,6 +28,15 @@ export class AppComponent implements OnInit {
       }
     })
   }
+
+  previousDay = computed<Params>(() => {
+    return {"day": this.persistence.previousDay(this.day())};
+  })
+
+
+  nextDay = computed<Params>(() => {
+    return {"day": this.persistence.nextDay(this.day())};
+  })
 
   private today(): string {
     return new Date().toLocaleDateString("de-DE", {day: "2-digit", month: "2-digit", year: "numeric"});
