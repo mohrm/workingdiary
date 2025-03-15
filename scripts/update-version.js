213dd9517e8005728890c1d613cbd21f4d646ad0
Martin Mohr <martin.mohr@posteo.de>
2025-03-15 11:00:58 +0100

    feat: include git version info into app bundle

---
+++ scripts/update-version.js
@@ -0,0 +1,14 @@
+const fs = require("fs");
+const execSync = require("child_process").execSync;
+
+const version = require("../package.json").version;
+const commitHash = execSync("git rev-parse --short HEAD").toString().trim();
+const commitTimestamp = execSync("printf %s $(git show --no-patch --format=%ci)");
+
+const content = `export const version = '${version}';
+export const commitTimestamp = '${commitTimestamp}';
+export const commitHash = '${commitHash}';
+export const urlOfLastCommit = 'https://github.com/mohrm/workingdiary/commit/${commitHash}';
+`
+
+fs.writeFileSync("./src/environments/version.prod.ts", content);
---
+++ src/environments/version.ts
@@ -0,0 +1,4 @@
+export const version = 'REPLACEME';
+export const commitTimestamp = 'REPLACEME';
+export const commitHash = '886cREPLACEMEeda';
+export const urlOfLastCommit = 'REPLACEME';
