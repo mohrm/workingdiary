97f8d97404728df80c328636ec9af5466f3082f1
Martin Mohr <martin.mohr@posteo.de>
2025-06-26 16:29:08 +0200

    fix: make update-version.js work correctly in windows and linux

--- scripts/update-version.js
+++ scripts/update-version.js
@@ -3,12 +3,12 @@ const execSync = require("child_process").execSync;
 
 const version = require("../package.json").version;
 const commitHash = execSync("git rev-parse --short HEAD").toString().trim();
-const commitTimestamp = execSync("printf %s \"$(git show --no-patch --format=%ci)\"");
+const commitTimestamp = execSync("git show --no-patch --format=%ci").toString().trim();
 
 const content = `export const version = '${version}';
 export const commitTimestamp = '${commitTimestamp}';
 export const commitHash = '${commitHash}';
 export const urlOfLastCommit = 'https://github.com/mohrm/workingdiary/commit/${commitHash}';
-`
+`;
 
 fs.writeFileSync("./src/environments/version.prod.ts", content);
