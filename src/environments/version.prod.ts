be58c524c20b46d4493352f4448bf56263ae0d23
Martin Mohr <martin.mohr@posteo.de>
2026-02-08 23:25:54 +0100

    Fix section list flex sizing and add version placeholder

---
+++ src/environments/version.prod.ts
@@ -0,0 +1,4 @@
+export const version = '0.0.0-dev';
+export const commitTimestamp = '1970-01-01 00:00:00 +0000';
+export const commitHash = 'dev';
+export const urlOfLastCommit = 'https://github.com/mohrm/workingdiary';
