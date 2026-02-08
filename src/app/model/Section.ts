f26f8309b4d1a558b86eba6f61c5a2575e29f03c
Martin Mohr <martin.mohr@posteo.de>
2026-02-08 18:12:45 +0100

    Add location to time sections

--- src/app/model/Section.ts
+++ src/app/model/Section.ts
@@ -3,14 +3,20 @@ import { Time } from './Time';
 export class Section {
   startTime: Time;
   endTime: Time;
+  location: string;
 
-  constructor(startTime: Time, endTime: Time) {
+  constructor(startTime: Time, endTime: Time, location: string = '') {
     this.startTime = startTime;
     this.endTime = endTime;
+    this.location = location;
   }
 
   formattedString(): string {
-    return this.startTime.formattedString() + " - " + this.endTime.formattedString();
+    const timeRange = this.startTime.formattedString() + " - " + this.endTime.formattedString();
+    if (this.location) {
+      return `${timeRange} (${this.location})`;
+    }
+    return timeRange;
   }
 
   durationInMinutes(): number {
@@ -18,10 +24,10 @@ export class Section {
   }
 
   toJSON(): any {
-    return {startTime: this.startTime, endTime: this.endTime}
+    return {startTime: this.startTime, endTime: this.endTime, location: this.location}
   }
 
-  static fromJSON(s: {startTime: any, endTime: any}): Section {
-    return new Section(Time.fromJSON(s.startTime), Time.fromJSON(s.endTime));
+  static fromJSON(s: {startTime: any, endTime: any, location?: string}): Section {
+    return new Section(Time.fromJSON(s.startTime), Time.fromJSON(s.endTime), s.location ?? '');
   }
 }
