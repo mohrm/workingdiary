c57ec556e9ece08eba0897d2c8168ba78bcb0896
Martin Mohr <martin.mohr@posteo.de>
2026-02-08 22:12:07 +0100

    Adjust industry time table layout and fix Jest setup (#319)

--- src/app/feature/day-plan/abschnitt/abschnitt.component.spec.ts
+++ src/app/feature/day-plan/abschnitt/abschnitt.component.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { AbschnittComponent } from './abschnitt.component';
+import { Section } from '../../../model/Section';
+import { Time } from '../../../model/Time';
 
 describe('AbschnittComponent', () => {
   let component: AbschnittComponent;
@@ -14,6 +16,10 @@ describe('AbschnittComponent', () => {
 
     fixture = TestBed.createComponent(AbschnittComponent);
     component = fixture.componentInstance;
+    fixture.componentRef.setInput(
+      'section',
+      new Section(new Time(9, 0), new Time(10, 0), 'Büro')
+    );
     fixture.detectChanges();
   });
 
