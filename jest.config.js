a934a235446ee01f5daa72e1cf798e290e1c40f2
Martin Mohr <martin.mohr@posteo.de>
2026-03-08 15:46:10 +0100

    make jest ignore cucumber artifacts

--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,6 @@ export default {
   preset: 'jest-preset-angular',
   testEnvironment: 'jsdom',
   setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
+  testMatch: ['<rootDir>/src/**/*.spec.ts'],
   transformIgnorePatterns: ['node_modules/(?!.*\\.mjs$)'],
 };
