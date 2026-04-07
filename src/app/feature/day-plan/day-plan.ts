5ce572a45f9d9477a6ed2dc98ef50a9776b9c779
Martin Mohr <martin.mohr@posteo.de>
2026-04-07 20:38:47 +0100

    Fix lint issues in source instead of disabling rules (#390)

--- src/app/feature/day-plan/day-plan.ts
+++ src/app/feature/day-plan/day-plan.ts
@@ -1,23 +1,17 @@
-import { Component, input, WritableSignal, signal, OnInit, computed, EventEmitter} from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { Time } from '../../model/Time';
+import { Component, EventEmitter, input } from '@angular/core';
 import { Section } from '../../model/Section';
 import { StempeluhrComponent } from './stempeluhr/stempeluhr.component';
 import { AbschnittListeComponent } from "./abschnitt-liste/abschnitt-liste.component";
 
 @Component({
-  selector: 'day-plan',
+  selector: 'app-day-plan',
   imports: [StempeluhrComponent, AbschnittListeComponent],
   templateUrl: './day-plan.html',
   styleUrl: './day-plan.css'
 })
-export class DayPlan implements OnInit {
+export class DayPlan {
 
   day = input.required<string>();
-
-  ngOnInit() {
-    
-  }
 
   stempelUhrEreignis : EventEmitter<Section>
   abschnitteAenderungsEreignis : EventEmitter<Section[]>
