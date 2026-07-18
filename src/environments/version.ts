501df0713a950802b955474f113cbd7f5ecc1657
Martin Mohr <martin.mohr@posteo.de>
2026-07-18 15:27:22 +0200

    fix: make version.ts a generated file (gitignored, auto-created on dev/test/build)

--- src/environments/version.ts
+++
@@ -1,4 +0,0 @@
-export const version = '0.0.0';
-export const commitTimestamp = '2026-07-18 15:20:04 +0200';
-export const commitHash = '81b3f14';
-export const urlOfLastCommit = 'https://github.com/mohrm/workingdiary/commit/81b3f14';
